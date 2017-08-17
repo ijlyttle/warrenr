61ba55d5c9e36257da96062a6c3a4749858ead29
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-08-17 08:25:20 -0500

    Copies `persistently()` from https://github.com/richierocks/purrr/commit/72f4cee203055cde5cf143e5c7e95089c92a2c18
    Removes httr dependency from documentation

---
+++ R/persistently.R
@@ -0,0 +1,79 @@
+#' Capture side effects.
+#'
+#' These functions wrap functions so that instead of generating side effects
+#' through printed output, messages, warnings, and errors, they return enhanced
+#' output. They are all adverbs because they modify the action of a verb (a
+#' function).
+#'
+#' @param .f A function, formula, or atomic vector. See [purrr::map()]
+#' @param quiet Hide errors (`TRUE`, the default), or display them
+#'   as they occur?
+#' @param otherwise Default value to use when an error occurs.
+#' @param max_attempts Positive integer. \code{persistent} functions will try
+#'   to run this many times before giving up.
+#' @param wait_seconds Positive number. Base multiplier for time in seconds to
+#'   wait between attempts. The time increases exponentially, with a wait time
+#'   randomly chosen from a uniform distribution between \code{0} and
+#'   \code{wait_seconds * 2 ^ (i - 1)} senconds, between the \code{i}th and
+#'   \code{i + 1}th attempts.
+#'
+#' @seealso \code{httr::RETRY()} is a special case of \code{persistently()}
+#' @return wrapped function uses a default value (`otherwise`)
+#'   whenever an error occurs max_attempts times.
+#' @export
+#' @examples
+#'
+#' # persistently() makes a function repeatedly try to work
+#'
+#' risky_runif <- function(lo = 0, hi = 1) {
+#'   y <- stats::runif(1, lo, hi)
+#'   if(y < 0.9) {
+#'     stop(y, " is too small")
+#'   }
+#'   y
+#' }
+#' persistent_risky_runif <- persistently(
+#'   risky_runif, otherwise = -99, quiet = FALSE, wait_seconds = 0.01)
+#' set.seed(1)
+#' persistent_risky_runif()
+#' set.seed(3)
+#' persistent_risky_runif()
+#'
+#'
+#' @export
+#'
+persistently <- function(.f, otherwise = NULL, quiet = TRUE, max_attempts = 5, wait_seconds = 0) {
+
+  .f <- purrr::as_mapper(.f)
+
+  force(otherwise)
+  force(quiet)
+  force(max_attempts)
+  force(wait_seconds)
+
+  function(...) {
+    for (i in seq_len(max_attempts)) {
+      answer <- purrr:::capture_error(.f(...), quiet = quiet)
+      if (is.null(answer$error)) {
+        return(answer$result)
+      }
+      if (wait_seconds > 0) {
+        actual_wait_seconds <- stats::runif(1, 0, wait_seconds * 2 ^ (i - 1))
+        if (!quiet) {
+          message(sprintf("Retrying in %.3g seconds.", actual_wait_seconds))
+        }
+        Sys.sleep(actual_wait_seconds)
+      }
+    }
+    if (!quiet) {
+      msg <- sprintf(
+        "%s failed after %d tries; returning %s.",
+        deparse(match.call()),
+        max_attempts,
+        format(otherwise)
+      )
+      message(msg)
+    }
+    otherwise
+  }
+}
